8ceaf9e86bd4eaa6dcf3b0a188158e941574b7e7
Phosphorus <fernando.n.p.d.moscu@gmail.com>
2023-09-18 02:20:54 -0300

    feat: add og info

--- src/components/layout.rs
+++ src/components/layout.rs
@@ -1,3 +1,4 @@
+use chrono::Datelike;
 use leptos::*;
 
 use crate::{
@@ -8,11 +9,31 @@ use crate::{
 #[component]
 // This is a common Layout component that will be used by all pages.
 pub fn Layout(children: Children) -> impl IntoView {
+    let year = chrono::Utc::now().year();
+
     view! {
         <Html attrs=vec![("lang", "es")]/>
-        <Title>"RustLang Es | Blog"</Title>
+        <Title>{format!("Blog de Rust Lang en Español {year}")}</Title>
         <Head>
             <meta charset="utf-8"/>
+            <meta property="og:site_name" content=format!("Blog de Rust Lang en Español {year}")/>
+            <meta property="og:title" content=format!("Blog de Rust Lang en Español {year}")/>
+            <meta
+                property="og:description"
+                content="Somos una comunidad de Rust hispana, buscamos la promoción del lenguaje de programación Rust."
+            />
+            <meta
+                name="description"
+                content="Somos una comunidad de Rust hispana, buscamos la promoción del lenguaje de programación Rust."
+            />
+            <meta property="og:url" content="https://rustlanges.github.io"/>
+            <meta property="og:image" content="https://rustlanges.github.io/preview_concept.png"/>
+            <meta
+                property="twitter:image"
+                content="https://rustlanges.github.io/preview_concept.png"
+            />
+            <meta name="twitter:card" content="summary_large_image"/>
+            <meta name="twitter:site" content="@rustlang"/>
             {if cfg!(debug_assertions) {
                 view! { <link rel="stylesheet" href="/output.css"/> }
             } else {
