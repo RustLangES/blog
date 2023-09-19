7acdf8134fd41e20f417ef1d22135a983581540d
Phosphorus <fernando.n.p.d.moscu@gmail.com>
2023-09-19 02:52:17 -0300

    feat: add comments

--- src/pages/article_page.rs
+++ src/pages/article_page.rs
@@ -9,19 +9,21 @@ use crate::{
 #[component]
 pub fn ArticlePage(article: Article) -> impl IntoView {
     let title = article.title.clone();
-    view! {<>
-        <Title>{title}</Title>
-        <Layout>
-            <BlogContent article=article/>
-        </Layout>
-        <script src="https://utteranc.es/client.js"
-        repo="RustLangES/blog"
-        issue-term="title"
-        label="comentarios 💬"
-        theme="github-light"
-        crossorigin="anonymous"
-        async>
-</script>
-</>
+    view! {
+        <>
+            <Title>{title}</Title>
+            <Layout>
+                <BlogContent article=article/>
+            </Layout>
+            <script
+                src="https://utteranc.es/client.js"
+                repo="RustLangES/blog"
+                issue-term="title"
+                label="comentarios 💬"
+                theme="github-light"
+                crossorigin="anonymous"
+                async
+            ></script>
+        </>
     }
 }
