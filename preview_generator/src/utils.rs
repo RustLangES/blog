f78eceaaee54c88bd2c59c08bc43fc1b644b704a
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2023-11-20 05:22:22 -0400

    feat: add blend image on insertion

--- preview_generator/src/utils.rs
+++ preview_generator/src/utils.rs
@@ -1,3 +1,5 @@
+use image::{Pixel, RgbaImage};
+
 pub fn chunked_string(s: String, size: usize, max: usize) -> Vec<String> {
     s.split_whitespace()
         .into_iter()
@@ -15,4 +17,22 @@ pub fn chunked_string(s: String, size: usize, max: usize) -> Vec<String> {
             }
         })
         .collect::<Vec<String>>()
+}
+
+pub fn append_image(img: &mut RgbaImage, other: &RgbaImage, x_min: u32, y_min: u32, new_alpha: u8) {
+    let mut logo_x = 0;
+    let mut logo_y;
+    for x in x_min..x_min + other.width() {
+        logo_y = 0;
+        for y in y_min..y_min + other.height() {
+            let pixel = other.get_pixel(logo_x, logo_y);
+            logo_y += 1;
+            if pixel.0[3] == 0 {
+                continue;
+            }
+            let old_pixel = img.get_pixel_mut(x, y);
+            old_pixel.blend(&pixel.map_with_alpha(|c| c, |_| new_alpha));
+        }
+        logo_x += 1;
+    }
 }
