339933fa1cdff5e88fbc9cde776d80def02bd2a9
krovuxdev <62192487+krovuxdev@users.noreply.github.com>
2024-03-25 09:48:41 -0500

    fix: move 'fontsize' to extend
    Co-authored-by: Phosphorus <fernando.n.p.d.moscu@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,28 +11,17 @@ module.exports = {
       "fira-sans": ["Fira Sans", "sans-serif"],
       "work-sans": ["Work Sans", "sans-serif"],
     },
-  fontSize: {
-      sm: '0.8rem',
-      base: '1rem',
-      xl: '1.25rem',
-      '2xl': '1.563rem',
-      '3xl': '1.953rem',
-      '4xl': '2.441rem',
-      '5xl': '3.052rem',
-      'half': '3.5rem',
-      '3.5xx': '3.5rem',
-    },
     extend: {
       screens: {
         'xs': '475px',
         ...defaultTheme.screens,
       },
-       lineHeight: {
-        'extra-loose': '2.5',
-        '12': '3rem',
-        '3lh': '2.8rem',
+      fontSize: {
+          'half': '3.5rem',
       },
-  
+      lineHeight: {
+          '3lh': '2.8rem',
+      },
       backgroundImage: (theme) => ({
         "anuncios": "url('https://i.imgur.com/tDlT9sr.jpg')",
         "kaku-dev": "url('https://www.rustlang-es.org/kaku.avif')",
